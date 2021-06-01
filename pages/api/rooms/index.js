b415f72a960841a1278455b677ff7d9207fa1264
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-06-01 13:40:54 -1000

    Section 6

--- pages/api/rooms/index.js
+++ pages/api/rooms/index.js
@@ -2,12 +2,14 @@ import nc from 'next-connect';
 import dbConnect from '../../../config/dbConnect';
 import { allRooms, newRoom } from '../../../controllers/roomControllers';
 
-const handler = nc();
+import onError from '../../../middlewares/errors';
+
+const handler = nc({ onError });
 
 dbConnect();
 
 handler.get(allRooms);
 
-handler.post(newRoom)
+handler.post(newRoom);
 
 export default handler;
